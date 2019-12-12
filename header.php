a85005ddf41b036f7a2789b9b06e2d3122b041bb
nohjsousa <jlimalion7@gmail.com>
2019-12-11 22:25:09 -0300

    O Loop

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 		<header>
 			<nav>
 				<ul>
-					<li class="current_page_item"><a href="/testewp.local/">Menu</a></li>
+					<li class="current_page_item"><a href="/">Menu</a></li>
 					<li><a href="/testewp.local/sobre">Sobre</a></li>
 					<li><a href="/testewp.local/contato">Contato</a></li>
 				</ul>
